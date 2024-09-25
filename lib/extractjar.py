c3d3ad1b27d69e0b03b8f13870d66f8b42970d3c
Bradley <104906904+bradley-03@users.noreply.github.com>
2024-09-25 20:45:19 +0100

    feat: cleaning up after jar extraction

--- lib/extractjar.py
+++ lib/extractjar.py
@@ -1,11 +1,20 @@
+import os
 import zipfile
+import shutil
 
 def extract_jar (verName):
     try:
+        if os.path.exists('./jars/data'):
+            print("Deleting existing recipes")
+            shutil.rmtree('./jars/data')
+
         with zipfile.ZipFile(f'./jars/{verName}.jar', 'r') as zip_ref:
             for member in zip_ref.namelist():
                 if member.startswith("data/minecraft/recipe/"):
                     zip_ref.extract(member, './jars')
+
+        print("Finished extracting! Cleaning up jar directory.")
+        os.remove(f'./jars/{verName}.jar')
     except Exception as e:
         print(e)
         print("Something went wrong while extracting jar file.")
